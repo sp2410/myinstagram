32287409d7aff41d6bcc1f5643bbdbeadc4ebf87
sp2410 <saprakas@syr.edu>
2016-12-16 13:09:40 -0500

    crud action added except delete

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -1,5 +1,12 @@
 class PicsController < ApplicationController
+	before_action :find_pic, only: [:show, :edit, :update, :destory]
+
 	def index
+		@pics = Pic.all.order("created_at DESC")
+	end
+
+	def show		
+
 	end
 
 	def new
@@ -8,6 +15,30 @@ class PicsController < ApplicationController
 
 	def create
 		@pic = Pic.new(pic_params)
+
+		if @pic.save
+			redirect_to @pic, notice: "Yesss! It was Posted"
+		else
+			render 'new'
+		end
+	end
+
+	def edit		
+	end
+
+	def update
+		if @pic.update(pic_params)
+			redirect_to @pic, notice: "Congrats! Pic is updated"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		if @pic.present?
+			@pic.destroy
+		end
+		redirect_to root_path
 	end
 
 	private
@@ -16,6 +47,11 @@ class PicsController < ApplicationController
 		params.require(:pic).permit(:title, :description)
 	end
 
+	def find_pic
+		@pic = Pic.find(params[:id])
+	end
+
 	
 
 end
+
