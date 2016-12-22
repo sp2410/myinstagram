b118f7fa11e6951cda3d88dd5863de882e59bd3d
sp2410 <saprakas@syr.edu>
2016-12-22 16:53:29 -0500

    version with image and bootstrap but resizing not working yet

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'haml', '~> 4.0.5'
 gem 'simple_form', '~> 3.0.2'
 gem 'bootstrap-sass', '~> 3.2.0.2'
 gem 'devise', '~> 3.4.1'
+gem 'paperclip', '~> 4.2.0'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
--- test/models/pic_test.rb
+++ test/models/pic_test.rb
@@ -4,4 +4,16 @@ class PicTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should not save pic without title" do
+  	pic = Pic.new
+  	assert_not pic.save, "Saved the pic without a title"
+  end
+
+  test "should report error" do
+  	assert_raises(NameError) do
+  		some_undefined_variable
+  	end
+  end
+
+
 end
